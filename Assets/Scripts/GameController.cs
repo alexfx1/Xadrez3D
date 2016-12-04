1284b953dbc8d376baf7eb4f78110131505ebfe3
Alexandre Savastano <xandy0210@gmail.com>
2016-12-04 13:25:39 -0200

    Instanciando o rei branco na cena

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using tabuleiro;
+using xadrex;
+
+class GameController : MonoBehaviour {
+
+    public GameObject reiBranco = null;
+
+    PartidaDeXadrez partida;
+
+	void Start () {
+        partida = new PartidaDeXadrez();
+        Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco); 
+	}
+}
