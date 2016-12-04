1d03e406b2f858ddf144605007c80455c72f14bc
Alexandre Savastano <xandy0210@gmail.com>
2016-12-04 17:30:12 -0200

    Criando o painel de mensagens da partida

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,11 +5,14 @@ using xadrex;
 class GameController : MonoBehaviour {
 
     public GameObject reiBranco = null;
+    public GameObject reiPreto = null;
 
     PartidaDeXadrez partida;
 
 	void Start () {
         partida = new PartidaDeXadrez();
-        Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco); 
-	}
+        Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco);
+        Util.instanciarRei('e', 8, Cor.Preta, partida, reiPreto); 
+    }
+
 }
